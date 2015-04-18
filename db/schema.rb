3daa1620ed18b7a3f094508af1fee1f00bbdd0ee
Sam Moore <samandmoore@gmail.com>
2015-04-18 17:38:31 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,6 @@ ActiveRecord::Schema.define(version: 20150418205608) do
     t.string   "company_name"
     t.string   "cusip"
     t.integer  "sector_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
